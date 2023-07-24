3bc5c733386e620f0da9107842a6b4a8fdf77974
JoelOlivo <139314609+JoelOlivo@users.noreply.github.com>
2023-07-24 16:17:22 -0500

    Crear menu

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('welcome');
+        return view('home');
     }
 
     public function home(){
